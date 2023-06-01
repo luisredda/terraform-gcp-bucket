78f78205da22687c65e50508b121abfcde58471f
Luis Redda <luis.redda@gmail.com>
2023-06-01 11:11:25 -0300

    Create gcpbucket.tf

---
+++ gcpbucket.tf
@@ -0,0 +1,13 @@
+provider "google" {
+  project = var.project_id
+  
+}
+
+resource "google_storage_bucket" "default" {
+
+  name = var.bucket_name
+  storage_class = var.storage_class
+  location = var.bucket_location
+ 
+  public_access_prevention = "enforced"
+}
